940c267ee9dd68de55085a6b64f6fcdb1fc8a70f
nnbaitei <nnbaitei@gmail.com>
2024-07-24 21:49:50 +0700

    update layout and map

--- detail.py
+++ detail.py
@@ -4,14 +4,21 @@ import dash
 from dash.dependencies import ALL
 import plotly.express as px
 import re
+import dash_leaflet as dl
 
-# โหลดข้อมูลจากไฟล์ CSV
 path = "data/data.csv"
 df = pd.read_csv(path)
 
-# ทำความสะอาดข้อมูลหลักสูตร
 df['course'] = df['course'].str.replace(r'^\d+\.\s*', '', regex=True)
 courses = df['course'].drop_duplicates()
+
+
+location_data = pd.read_csv(path)
+
+# Filter data for locations that have latitude and longitude
+location_data = location_data.dropna(subset=['latitude', 'longitude'])
+
+text_color = 'black'
 
 def extract_number(text):
     numbers = re.findall(r'\d+', text)
@@ -24,36 +31,47 @@ app = Dash(__name__, external_stylesheets=['/assets/style.css'])
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
-    html.Div([      
-        html.Div([
-            html.Div([html.H3("สาขา", className="card-title")], style={'fontSize': '40px', 'color': 'black'}),
-            dcc.Dropdown(courses, id='course-input', placeholder="กรุณาเลือกสาขา", style={'width': '300px', 'color': 'black'}, value='วิศวกรรมปัญญาประดิษฐ์'),
-        ], style={'display': 'flex', 'flexDirection': 'row', 'gap': '20px', 'alignItems': 'center', 'margin': '20px', 'marginBottom': '5px'}),
-        
-        html.Div([
-            html.Div([html.H3("หลักสูตร", className="card-title")], style={'alignItems': 'center', 'justifyContent': 'center', 'fontSize': '25px'}),
-            html.Div(id='sub-course-list')
-        ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': '#FF8225', 'width': '500px', 'height': '400px', 'overflowY': 'scroll', 'padding': '20px', 'paddingBottom': '40px', 'borderRadius': '25px', 'margin': '20px', 'alignItems': 'center', 'marginTop': '10px', 'color': 'white'}),
-        
-        html.Div([
-            html.Div([html.H3("มหาวิทยาลัย", className="card-title")], style={'fontSize': '25px', 'color': 'white'}),
-            html.Div([html.Div(id='university-output')], style={'fontSize': '25px'})
-        ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': '#B43F3F', 'width': '500px', 'height': '180px', 'padding': '20px', 'paddingBottom': '30px', 'borderRadius': '25px', 'marginBottom': '10px', 'margin': '20px', 'alignItems': 'center', 'justifyContent': 'center'}),
-    ], style={'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'center', 'textAlign': 'center'}),
     html.Div([
-        html.Div([
-            html.Div([html.H3("ข้อมูลการรับสมัคร", className="card-title")], style={'justifyContent': 'center', 'textAlign': 'center', 'color': 'white'}),
-            dcc.Graph(id='bar-graph'),
+            html.Div([html.H3("สาขา", className="card-title")], style={'fontSize': '40px', 'color': text_color}),
+            dcc.Dropdown(courses, id='course-input', placeholder="กรุณาเลือกสาขา", style={'width': '300px', 'color': 'black'}, value='วิศวกรรมปัญญาประดิษฐ์'),
+        ], style={'display': 'flex', 'flexDirection': 'row', 'gap': '20px', 'alignItems': 'center', 'marginTop': '20px'}),
 
-        ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': '#173B45', 'width': '500px', 'height': '550px', 'padding': '10px', 'paddingBottom': '20px', 'borderRadius': '25px', 'margin': '20px', 'fontSize': '25px'}),
+    html.Div([
+        html.Div([      
+            html.Div([
+                html.Div([html.H3("หลักสูตร", className="card-title")], style={'alignItems': 'center', 'justifyContent': 'center', 'fontSize': '25px'}),
+                html.Div(id='sub-course-list')
+            ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': 'rgba(147, 177, 166, 0.8)', 'width': '500px', 'height': '470px', 'overflowY': 'scroll', 'padding': '20px', 'paddingBottom': '40px', 'borderRadius': '25px', 'marginRight': '20px', 'alignItems': 'center', 'color': text_color}),
+
+        ], style={'display': 'flex', 'flexDirection': 'column', 'textAlign': 'center'}),
         html.Div([
-            html.Div([html.H3("ค่าธรรมเนียม", className="card-title")], style={'fontSize': '25px'}),
-            html.Div([html.Div(id='fee-info')], style={'fontSize': '25px'})
-        ], style={'backgroundColor': '#FF8225', 'width': '480px', 'height': '200px', 'padding': '20px', 'paddingBottom': '40px', 'borderRadius': '25px', 'alignItems': 'center', 'marginTop': '60px', 'marginBottom': '10px', 'marginLeft': '20px', 'color': 'white', 'textAlign': 'center'})
-    ])
-    
-    
-], style={'display': 'flex', 'flexDirection': 'row'})
+            html.Div([
+                html.Div([html.H3("ข้อมูลการรับสมัคร", className="card-title")], style={'justifyContent': 'center', 'textAlign': 'center', 'color': text_color}),
+                dcc.Graph(id='bar-graph'),
+
+            ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': 'rgb(24, 61, 61, 0.6)', 'width': '500px', 'height': '500px', 'padding': '10px', 'paddingBottom': '20px', 'borderRadius': '25px', 'marginRight': '20px', 'fontSize': '25px'}),
+        ]),
+        html.Div([
+            html.Div([
+                html.Div([html.H3("มหาวิทยาลัย", className="card-title")], style={'fontSize': '25px', 'color': text_color}),
+                html.Div([html.Div(id='university-output')], style={'fontSize': '25px'})
+            ], style={'display': 'flex', 'flexDirection': 'column', 'backgroundColor': '#93B1A6', 'width': '500px', 'height': '200px', 'padding': '20px', 'paddingBottom': '30px', 'borderRadius': '25px', 'marginLeft': '20px', 'marginBottom': '20px', 'alignItems': 'center', 'justifyContent': 'center'}),
+            html.Div([
+                html.Div([html.H3("ค่าธรรมเนียม", className="card-title")], style={'fontSize': '25px'}),
+                html.Div([html.Div(id='fee-info')], style={'fontSize': '25px'})
+            ], style={'backgroundColor': '#5C8374', 'width': '500px', 'height': '200px', 'padding': '20px', 'paddingBottom': '40px', 'borderRadius': '25px', 'alignItems': 'center', 'marginLeft': '20px', 'color': text_color, 'textAlign': 'center'}),
+        ])
+
+    ], style={'display': 'flex', 'flexDirection': 'row', 'justifyContent': 'center'}),
+    html.Div([
+        html.H1("ตำแหน่ง", style={'textAlign': 'center'}),
+        dl.Map(id='map', center=[13.736717, 100.523186], zoom=5, style={'height': '303px', 'width': '194vh'}, children=[
+        dl.TileLayer(),
+        dl.LayerGroup(id="markers")
+        ])    
+    ], style={'marginTop': '40px'})
+
+], style={'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'center', 'alignItems': 'center'})
 
 @app.callback(
     Output('sub-course-list', 'children'),
@@ -80,7 +98,10 @@ def update_sub_course_list(value):
 @app.callback(
     [Output('university-output', 'children'),
      Output('bar-graph', 'figure'),
-     Output('fee-info', 'children')],
+     Output('fee-info', 'children'),
+     Output('map', 'center'),
+     Output('map', 'zoom'),
+     Output('markers', 'children')],
     [Input({'type': 'sub-course-button', 'index': ALL}, 'n_clicks'),
      Input('course-input', 'value')]
 )
@@ -91,9 +112,12 @@ def update_university_output_and_bar_graph(n_clicks, course_value):
     university_output = "กรุณาเลือกหลักสูตร"
     bar_figure = {}
     fee_info = []
+    markers = []
+    map_center = [13.736717, 100.523186]  # Default center coordinates
+    map_zoom = 5  # Default zoom level
 
     if not ctx.triggered:
-        return university_output, bar_figure, fee_info
+        return university_output, bar_figure, fee_info, map_center, map_zoom, markers
 
     triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
     if 'index' in triggered_id:
@@ -102,7 +126,7 @@ def update_university_output_and_bar_graph(n_clicks, course_value):
         university = df[df['sub_course'] == sub_course]['university']
 
         if university.empty:
-            return university_output, bar_figure, fee_info
+            return university_output, bar_figure, fee_info, map_center, map_zoom, markers
 
         first_university = university.iloc[0]
 
@@ -129,17 +153,17 @@ def update_university_output_and_bar_graph(n_clicks, course_value):
         fig.update_layout(
             plot_bgcolor='rgba(0,0,0,0)',  # Transparent plot background
             paper_bgcolor='rgba(0,0,0,0)',  # Transparent paper background
-            title_font=dict(size=18, color='white'),
-            xaxis_title_font=dict(size=14, color='white'),
-            yaxis_title_font=dict(size=14, color='white'),
-            xaxis=dict(color='white'),
-            yaxis=dict(color='white')
+            title_font=dict(size=18, color=text_color),
+            xaxis_title_font=dict(size=14, color=text_color),
+            yaxis_title_font=dict(size=14, color=text_color),
+            xaxis=dict(color=text_color),
+            yaxis=dict(color=text_color)
         )
 
         fig.update_traces(marker_color='#F8EDED')
         fig.update_xaxes(
             showline=True, 
-            linecolor='white',
+            linecolor=text_color,
             gridcolor='rgba(0,0,0,0)'  # Transparent grid lines
         )
         fig.update_yaxes(
@@ -152,11 +176,28 @@ def update_university_output_and_bar_graph(n_clicks, course_value):
         fee_info = [html.P(f'{amount}') for amount in fee_data]
 
         university_output = html.P(first_university)
+        
+        # Filter location data for the selected university
+        location_data_filtered = location_data[location_data['university'] == first_university]
+        if not location_data_filtered.empty:
+            first_location = location_data_filtered.iloc[0]
+            map_center = [first_location['latitude'], first_location['longitude']]
+            map_zoom = 12  # Zoom in closer to the university
 
-        return university_output, fig, fee_info
+            markers = [
+                dl.Marker(position=[row['latitude'], row['longitude']], children=[
+                    dl.Tooltip(row['university']),
+                    dl.Popup([
+                        html.H4(row['university']),
+                        html.P(f"Latitude: {row['latitude']}"),
+                        html.P(f"Longitude: {row['longitude']}")
+                    ])
+                    ]) for index, row in location_data_filtered.iterrows()
+                ]
 
-    return university_output, bar_figure, fee_info
+        return university_output, fig, fee_info, map_center, map_zoom, markers
 
+    return university_output, bar_figure, fee_info, map_center, map_zoom, markers
 
 if __name__ == '__main__':
     app.run(debug=True)
